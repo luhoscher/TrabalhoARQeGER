0a5767cfaaf66d23ac5700dceaae17da8320406d
luhoscher <luhoscher@gmail.com>
2017-11-27 21:26:31 -0200

    #7 Implementação termos e assuntos de busca
    Modelo implementado e iniciando a implementação e teste da inclusao

---
+++ SQLite/src/persistencia/jdbc/TermoJDBC.java
@@ -0,0 +1,107 @@
+package persistencia.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import persistencia.modelo.Termo;
+import persistencia.util.SQLite;
+
+public class TermoJDBC {
+
+    public void insert(Termo object) {
+        Connection c = null;
+        Statement declaracao = null;
+        try {
+            c = SQLite.iniciarConexao();
+            declaracao = c.createStatement();
+            String sql = "INSERT INTO SUBJECT (termo) "
+                    + "VALUES ('" + object.getTermo() + "');";
+            declaracao.executeUpdate(sql);
+            declaracao.close();
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        System.out.println("Criado com sucesso");
+    }
+
+    public boolean deletar(int id) {
+        Connection c = SQLite.iniciarConexao();
+        Statement declaracao = null;
+        try {
+            c = SQLite.iniciarConexao();
+            declaracao = c.createStatement();
+            return declaracao.executeUpdate("DELETE FROM termo WHERE id = " + id + ";") > 0;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
+        }
+    }
+
+    public Termo encontrar(int id) {
+        Connection c = SQLite.iniciarConexao();
+        Statement declaracao = null;
+        try {
+            c = SQLite.iniciarConexao();
+            declaracao = c.createStatement();
+            ResultSet rs = declaracao.executeQuery("SELECT * FROM termo WHERE id = " + id + ";");
+            Termo s = null;
+            while (rs.next()) {
+                int i = rs.getInt("id");
+                String termo = rs.getString("termo");
+                s = new Termo();
+                s.setId(i);
+                s.setTermo(termo);
+            }
+            rs.close();
+            declaracao.close();
+            return s;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return null;
+        }
+    }
+
+    public List<Termo> listar() {
+        Connection c = SQLite.iniciarConexao();
+        Statement declaracao = null;
+        try {
+            c = SQLite.iniciarConexao();
+            declaracao = c.createStatement();
+            ResultSet rs = declaracao.executeQuery("SELECT * FROM termo;");
+            List<Termo> termos = new ArrayList<>();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String termo = rs.getString("termo");
+                Termo s = new Termo();
+                s.setId(id);
+                s.setTermo(termo);
+                termos.add(s);
+            }
+            rs.close();
+            declaracao.close();
+            return termos;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return null;
+        }
+    }
+    
+    public boolean atualizar(Termo object) {
+        Connection c = null;
+        Statement declaracao = null;
+        try {
+            c = SQLite.iniciarConexao();
+            declaracao = c.createStatement();
+            String sql = "UPDATE SUBJECT SET "
+                    + "termo = '" + object.getTermo() + "'"
+                    + "WHERE id = " + object.getId() + ";";
+            return declaracao.executeUpdate(sql) > 0;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
+        }
+    }
+}
