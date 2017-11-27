42102eb593fc11eb0e19c9f98ec316a0df1cc384
luhoscher <luhoscher@gmail.com>
2017-11-27 21:28:53 -0200

    #13 Noticia e noticia jdbc
    Iniciando implementação da notícia e seu jdbc

---
+++ SQLite/src/persistencia/modelo/Noticia.java
@@ -0,0 +1,54 @@
+package persistencia.modelo;
+
+import java.util.Date;
+
+public class Noticia {
+
+    private int id;
+    private String url;
+    private String titulo;
+    private Date data;
+
+    public Noticia() {
+    }
+
+    public Noticia(int id, String url, String titulo, Date data) {
+        this.id = id;
+        this.url = url;
+        this.titulo = titulo;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+}
