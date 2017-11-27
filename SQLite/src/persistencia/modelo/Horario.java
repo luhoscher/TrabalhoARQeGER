3429c8bf854e7b53d175cd711b3c3b0b72a39446
luhoscher <luhoscher@gmail.com>
2017-11-27 21:28:10 -0200

    #10 Horario modelo e horário jbdc
    Iniciando a implementação do horário

---
+++ SQLite/src/persistencia/modelo/Horario.java
@@ -0,0 +1,33 @@
+package persistencia.modelo;
+
+import java.util.Date;
+
+public class Horario {
+
+    private int id;
+    private Date data;
+
+    public Horario() {
+    }
+
+    public Horario(int id, Date data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }    
+}
