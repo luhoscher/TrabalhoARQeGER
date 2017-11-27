e147b29e08ff8993bffbf5be638a097f91088af8
luhoscher <luhoscher@gmail.com>
2017-11-27 21:27:33 -0200

    #7 Modelo Termo
    Esqueci de adicionar no commit anterior

---
+++ SQLite/src/persistencia/modelo/Termo.java
@@ -0,0 +1,34 @@
+package persistencia.modelo;
+
+public class Termo {
+
+    private int id;
+    private String termo;
+
+    public Termo() {
+    }
+
+    public Termo(int id, String termo) {
+        this.id = id;
+        this.termo = termo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTermo() {
+        return termo;
+    }
+
+    public void setTermo(String termo) {
+        this.termo = termo;
+    }
+    
+    
+
+}
