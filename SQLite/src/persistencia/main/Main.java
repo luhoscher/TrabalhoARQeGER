e7d1e5f90b1e758153d0a568a65a51e3ca88d7ec
luhoscher <luhoscher@gmail.com>
2017-11-28 09:54:39 -0200

    #15 Implementação main para SQLite
    Implementação da classe main para o sqlite

---
+++ SQLite/src/persistencia/main/Main.java
@@ -0,0 +1,10 @@
+package persistencia.main;
+
+import persistencia.util.SQLite;
+
+public class Main {
+    public static void main(String[] args) {        
+        SQLite banco = new SQLite();         
+
+    }
+}
